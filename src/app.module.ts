b4d371abfaa11e42c599ca6563b8891cc344e77a
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-03-02 17:28:58 -0300

    ✨ New Feature:  Construção recurso postagem

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostagemModule } from './postagem/postagem.module';
+import { Postagem } from './postagem/entities/postagem.entity';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '7803luxO.',
+      database: 'db_blogpessoal',
+      entities: [Postagem],
+      synchronize: true,
+    }),
+    PostagemModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
